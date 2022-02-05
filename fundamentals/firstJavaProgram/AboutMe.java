4f0fe75d67c877f59e205493fd3fbbf2ba2ac52f
Nicole Gray <ngray122@gmail.com>
2022-02-05 16:30:24 -0500

    first java project

---
+++ fundamentals/firstJavaProgram/AboutMe.java
@@ -0,0 +1,8 @@
+public class AboutMe {
+      
+        public static void main(String[] args) {
+            System.out.println("My name is Nicole");
+            System.out.println("I am 35 years old");
+            System.out.println("My hometown is Millbury, MA");
+    }
+}
---
+++ fundamentals/greetings/Test.java
@@ -0,0 +1,12 @@
+
+
+// class name must always be the same as file name
+public class Test {
+        // function thst belongs to class
+            // public static void == function in js
+                // main() method is launching point for app. All prodcedural code will be wtitten between main 
+                // parameters have type declaration
+        public static void main(String[] args) {
+            System.out.println("Hello World");
+    }
+}
